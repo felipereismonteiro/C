06dc3cc79e655346969c845c8df39a6612ec6e0c
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2024-01-07 11:03:09 -0300

    feat: array lops

--- arrays/loops.c
+++ arrays/loops.c
@@ -6,9 +6,21 @@ int main() {
 
   sprintf(palavraSecreta, "MELANCIA");
 
+  int enforcou = 0;
+  int acertou = 0;
+  
+  do {
   for(int i = 0; i < strlen(palavraSecreta); i++) {
-    printf("%c", palavraSecreta[i]);
+    printf("_ ");
   } 
+    printf("\n");
+
+    char chute;
+
+    // se nao coloar o epaço antes ele simplesmente considera a letra e o enter entao ele imprime duas vezes o loop
+    scanf(" %c", &chute);
+  } while(!acertou && !enforcou);
+
   
   return 0;
 }
