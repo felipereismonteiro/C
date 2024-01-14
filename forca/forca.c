1c62cf1fc92bc255bdb5d5135039162aa4737705
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2024-01-14 11:00:19 -0300

    feat: memory addresses

--- forca/forca.c
+++ forca/forca.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
+/** Se coloca o * antes do tentativas para ser como ponteiro, agora no array de chutes nao se aplica pq 
+ * Isso ocorre porque o nome de um array em C, quando usado como argumento de uma função, já é um ponteiro para o primeiro elemento do array.
+ */
+
+/**Entao ao declarar com & eu estou apontando para a variavel q eu quero  chuteFunction(chutes, &tentativas, &chute); e ao pegar na funcao com o * eu estou usando o valor dessa variavel*/
+void chuteFunction(char chutes[26], int *tentativas, char *chute) {
+  printf("\nDigite seu chute: ");
+  scanf(" %c", chute);
+
+  printf("\n%c", chute);
+
+  chutes[*tentativas] = *chute;
+  (*tentativas)++;
+}
+
 int main() {
   char palavraSecreta[6] = "felipe";
 
@@ -14,10 +29,7 @@ int main() {
 
   do {
     char chute;
-    printf("\nDigite seu chute: ");
-    scanf(" %c", &chute);
-    chutes[tentativas] = chute;
-    tentativas++;
+    chuteFunction(chutes, &tentativas, &chute);
 
     acertos = 0;
 
