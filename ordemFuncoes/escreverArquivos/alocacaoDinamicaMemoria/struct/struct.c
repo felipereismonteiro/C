b36679f9f1b5ff9e52b2d30d158d030b5fab65df
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2024-01-25 12:59:29 -0300

    struct

---
+++ ordemFuncoes/escreverArquivos/alocacaoDinamicaMemoria/struct/struct.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct variaveis {
+ int idade;
+ char nomeLetra;
+ char cidadeLetra
+};
+
+int main() {
+ struct variaveis pessoa;
+
+ pessoa.idade = 21;
+
+ printf("%d", pessoa.idade);
+
+ return 0;
+}
