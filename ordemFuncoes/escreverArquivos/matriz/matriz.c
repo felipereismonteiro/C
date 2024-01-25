c98c64aed271bbd3dd9b56c37846e607611e682d
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2024-01-25 11:29:18 -0300

    matriz printf

--- ordemFuncoes/escreverArquivos/matriz/matriz.c
+++ ordemFuncoes/escreverArquivos/matriz/matriz.c
@@ -3,7 +3,10 @@
 int main() {
  char matriz[5][10];
 
- matriz[0][1] = '';
+ matriz[0][1] = 'a';
+
+ // devolve o array se passa apenas a primeira posição;
+ printf("%c", matriz[1]);
  
  return 0;
 }
