c882a0848080b897a9652cf3f6568187cce5dcb8
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2024-01-25 09:42:42 -0300

    fechando programa se der erro no banco de dados

--- ordemFuncoes/leitura-arquivos/leituraArquivos.c
+++ ordemFuncoes/leitura-arquivos/leituraArquivos.c
@@ -8,6 +8,12 @@ int main() {
  // assim ele le o arquivo por causa do r que no caso é read
  f = fopen("nomes.txt", "r");
 
+ if (f == 0) {
+  printf("Desculpe banco de dados nao disponivel");
+  // terminar o programa
+  exit(1);
+ }
+
  int quantidadeDePalavras;
  fscanf(f, "%d", &quantidadeDePalavras);
 
@@ -19,20 +25,8 @@ int main() {
  for(int i = 0; i < indiceRandomico; i++) {
   fscanf(f, "%s", nome);
  }
-
- /*
-  O problema principal aqui é que você está usando <= em vez de < na condição do loop. Isso significa que o loop continuará enquanto i for menor ou igual ao comprimento da string nome. No entanto, as strings em C são arrays de caracteres terminados por um caractere nulo (\0). Portanto, se nome contiver a palavra lida do arquivo, strlen(nome) dará o comprimento da palavra, mas não incluirá o caractere nulo no final. Assim, o loop irá além do final da palavra e provavelmente imprimirá caracteres inválidos da memória.
-
-Para corrigir isso, você deve usar < em vez de <= na condição do loop para garantir que você não ultrapasse o comprimento real da palavra. Aqui está a correção:
-
-c
+ printf("%s\n", nome);
  
- */
-
- for(int i = 0; i < strlen(nome); i++) {
-    printf("%c", nome[i]);
- }
-
  fclose(f);
 
  return 0;
